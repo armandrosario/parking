f8dd1a3d3cc00222fee1cfb4d1dffa4b0fe5cdb3
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-10-20 18:35:15 -0500

    Create car.java

---
+++ car.java
@@ -0,0 +1,30 @@
+public class Car {
+	private String licensePlate;
+	private Ticket ticket;
+	
+	public Car(String n) {
+		licensePlate=n;
+	}
+	public Car() {
+		
+	}
+	public Car getCar() {
+		return this;
+	}
+	public void setTicket(Ticket t) {
+		ticket=t;
+	}
+	public void leave(String t) {
+		System.out.println(licensePlate + ":leaving parking lot at" +t);
+		ticket=null;
+	}
+	public String getName() {
+		return licensePlate;
+	}
+	public void enter(String t) {
+		System.out.println(licensePlate+":entering parking lot at"+t);
+	}
+	public Ticket getTicket() {
+		return ticket;
+	}
+}
