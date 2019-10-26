eb3f41c6ee1b5155b33c8c698bf3aace76ab92cf
Armand Rosario <31975821+armandrosario@users.noreply.github.com>
2019-10-25 20:25:58 -0500

    Update parkinglot.java
    Added names for parking lots, added features to make multiple parking lots easier to manage

--- parkinglot.java
+++ parkinglot.java
@@ -5,15 +5,32 @@ public class ParkingLot {
 	private ArrayList<Car> cars=new ArrayList<Car>();
 	private int cost;
 	private int profit;
+	private String name;
 	
-	public ParkingLot(int c,int co) {
+	public ParkingLot(int c,int co,String n) {
+		name=n;
 		capacity=c;
 		totalCars=0;
 		cost=co;
-		System.out.println("Parking lot created.");
+		System.out.println("Parking lot " +name+ " created.");
 		System.out.println("Capacity:"+capacity);
 		System.out.println("Price:$"+cost);
 	}
+	public boolean isFull() {
+		if(totalCars==capacity) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public void showSpace() {
+		System.out.println("Lot " +name+" Available spots: "+(capacity-totalCars));
+	}
+	public ParkingLot() {}
 	ArrayList<Car> getCars(){
 		return cars;
 	}
@@ -36,25 +53,26 @@ public class ParkingLot {
 		System.out.println("Total profit:$"+profit);
 	}
 	public void print() {
+		System.out.println("Parking lot: "+name);
 		System.out.println("***Current Cars "+totalCars+"***");
 		for(int i=0;i<cars.size();i++) {
 			System.out.println((i+1)+". "+cars.get(i).getName());
 		}
 	}
-	public void leave(String name) {
+	public void leave(String n) {
 		boolean left=false;
 		Car c=new Car();
 		for(int i=0;i<cars.size();i++) {
-			if(cars.get(i).getName().equals(name)) {
+			if(cars.get(i).getName().equals(n)) {
 				c=cars.get(i);
 				cars.remove(i);
 				left=true;
 			}
 		}
 		if(left==true) {
-			System.out.print(c.getName()+" is leaving.");
-			System.out.print(" Time of entrance:"+c.getTicket().getTime());
-			System.out.println(" $"+c.getTicket().getPrice()+" made.");
+			System.out.print(c.getName()+" is leaving Lot "+name);
+			System.out.print(". Time of entrance: "+c.getTicket().getTime());
+			System.out.println(". $"+c.getTicket().getPrice()+" made.");
 			profit+=cost;
 			totalCars--;
 			if(totalCars==0) {
@@ -68,15 +86,15 @@ public class ParkingLot {
 	}
 	public void enter(Car car,String time) {
 		if(totalCars<capacity) {
-			System.out.println(car.getName()+" has entered at "+time);
+			System.out.println(car.getName()+" has entered Lot "+name+" at "+time);
 			cars.add(car);
 			totalCars++;
-			if(totalCars==capacity) {
-				System.out.println("Parking lot now full!");
+			if(isFull()) {
+				System.out.println("Parking lot "+name+ " now full!");
 			}
 		}
 		else {
-			System.out.println("Parking lot full Car " +car.getName()+" cannot enter.");
+			System.out.println("Parking lot "+ name+ " full Car " +car.getName()+" cannot enter.");
 		}
 	}
 	
